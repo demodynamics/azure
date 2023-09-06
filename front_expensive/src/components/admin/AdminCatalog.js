2afd57f12c898bff525cc43f9540f331f11e3bc6
root <root@demod124761.vds>
2023-09-06 17:42:44 +0000

    Initial commit

--- front_expensive/src/components/admin/AdminCatalog.js
+++ front_expensive/src/components/admin/AdminCatalog.js
@@ -29,7 +29,7 @@ function AdminCatalog() {
             navigate('/')
         }
 
-    }, [name, navigate])
+    }, [getCategory])
 
     function handleChange(e) {
         setItemPrice(e)
--- front_expensive/src/components/main/pages/Pages.js
+++ front_expensive/src/components/main/pages/Pages.js
@@ -2,7 +2,7 @@ import { useParams } from 'react-router-dom';
 import './Pages.css'
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { Button, Card, Form, Modal, Spinner,InputGroup } from 'react-bootstrap';
+import { Button, Card, Form, Modal, Spinner, InputGroup } from 'react-bootstrap';
 import * as Icon from 'react-bootstrap-icons'
 import { toast } from 'react-toastify';
 import NotFound from '../../../routing/NotFound';
@@ -10,15 +10,12 @@ import NotFound from '../../../routing/NotFound';
 function Pages() {
     const basketArr = []
     let { name } = useParams();
-    
+
     const [alko, setAlko] = useState()
-
-	const [images, setImages] = useState([])
-
     const [nameT, setNameT] = useState()
     const [phoneT, setPhoneT] = useState()
     const [show, setShow] = useState(false);
-	const [spanError, setSpanError] = useState('');
+    const [spanError, setSpanError] = useState('');
     const handleClose = () => {
         setNameT("")
         setPhoneT("")
@@ -26,27 +23,27 @@ function Pages() {
     };
     const handleShow = () => setShow(true);
 
-     async function getCategory() {
-         try {
-             const category = await axios.get(`http://localhost:5003/api/list/get-category/${name}`)
-             setAlko(category.data)
+    async function getCategory() {
+        try {
+            const category = await axios.get(`http://localhost:5003/api/list/get-category/${name}`)
+            setAlko(category.data)
         } catch (e) {
             console.log(e)
-         }
-     }
+        }
+    }
 
     useEffect(() => {
         getCategory()
-    }, [])
+    }, [getCategory])
 
     function addToBasket(e, item) {
         e.preventDefault()
 
         let filtered = []
-        if(localStorage.getItem('basket')) {
+        if (localStorage.getItem('basket')) {
             filtered = JSON.parse(localStorage.getItem('basket')).filter(item2 => item2.id === item.id)
         }
-        
+
 
         if (filtered.length > 0) {
             toast.info("Этот товар есть в корзине")
@@ -76,7 +73,7 @@ function Pages() {
         toast.success("Товар добавлен в корзину")
     }
 
-     const reg = /^\d{10}$/
+    const reg = /^\d{10}$/
 
     async function sendTelegram() {
         const TOKEN = "6104094925:AAEAcMEOPXFtUvUmgGejDzfl8HBNjLzH8X0"
@@ -108,50 +105,50 @@ function Pages() {
 
     }
 
-const validEndpoints = ["vodka", "beer", "whiskey", "cognac", "liquor", "tequilla", "jin", "rom", "wine", "champagne", "vermouth", "juice", "water", "snacks", "ciggarettes", "tobacandcalian", "sticks", "el-sig", "other"]
+    const validEndpoints = ["vodka", "beer", "whiskey", "cognac", "liquor", "tequilla", "jin", "rom", "wine", "champagne", "vermouth", "juice", "water", "snacks", "ciggarettes", "tobacandcalian", "sticks", "el-sig", "other"]
 
     const currentPath = window.location.pathname.split('/')[2]// Add other valid endpoints if needed
 
-    const isValidEndpoint = validEndpoints.some(endpoint => currentPath == endpoint);
+    const isValidEndpoint = validEndpoints.some(endpoint => currentPath === endpoint);
 
     return (
         <>
             {
-                !isValidEndpoint ? 
-			<NotFound />
-            :
-            <div className='pages_container'>
-            {
-                    alko ? alko.map((item, index) => {
-                        return (
-                            <Card className='pages_cards' key={index}>
-                                {item.oldPrice ? <Card.Text className='akcia-text'><img className='akcia_img' src='/image/akcia.png' /></Card.Text> : null}
-                                <Card.Img variant="top" className='item_img' src={`http://localhost:5003/api/imgs/${name}/${item.img}${/\.\w+$/.test(item.img) ? '' : '.jpg'}`} />
-                                <Card.Body className='card.body akcia-card-body'>
-                                    <Card.Title>{item.name}</Card.Title>
-                                    <Card.Text className='old-price'>{item.oldPrice ? item.oldPrice + " РУБ" : ""}</Card.Text>
-                                    <Card.Text style={{ color: "#21C737" }}>{item.price} РУБ</Card.Text>
-                                    <Button onClick={handleShow}>В 1 клик</Button>{' '}
-                                    <Button className='cart4_icon' onClick={(e) => addToBasket(e, item)}>
-                                        <Icon.Cart4 />
-                                    </Button>
-                                </Card.Body>
-                            </Card>
-                        )
-                    }) : <Spinner animation="border" variant="success" />
-                }
+                !isValidEndpoint ?
+                    <NotFound />
+                    :
+                    <div className='pages_container'>
+                        {
+                            alko ? alko.map((item, index) => {
+                                return (
+                                    <Card className='pages_cards' key={index}>
+                                        {item.oldPrice ? <Card.Text className='akcia-text'><img className='akcia_img' src='/image/akcia.png' /></Card.Text> : null}
+                                        <Card.Img variant="top" className='item_img' src={`http://localhost:5003/api/imgs/${name}/${item.img}${/\.\w+$/.test(item.img) ? '' : '.jpg'}`} alt="" />
+                                        <Card.Body className='card.body akcia-card-body'>
+                                            <Card.Title>{item.name}</Card.Title>
+                                            <Card.Text className='old-price'>{item.oldPrice ? item.oldPrice + " РУБ" : ""}</Card.Text>
+                                            <Card.Text style={{ color: "#21C737" }}>{item.price} РУБ</Card.Text>
+                                            <Button onClick={handleShow}>В 1 клик</Button>{' '}
+                                            <Button className='cart4_icon' onClick={(e) => addToBasket(e, item)}>
+                                                <Icon.Cart4 />
+                                            </Button>
+                                        </Card.Body>
+                                    </Card>
+                                )
+                            }) : <Spinner animation="border" variant="success" />
+                        }
 
-            </div>
-}
+                    </div>
+            }
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
                     <Modal.Title>Купить в 1 клик</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <Form>
-						 <Form.Label>Имя</Form.Label>
+                        <Form.Label>Имя</Form.Label>
                         <InputGroup className="mb-3">
-                           
+
                             <Form.Control
                                 type="text"
                                 required
@@ -160,9 +157,9 @@ const validEndpoints = ["vodka", "beer", "whiskey", "cognac", "liquor", "tequill
                                 onChange={(e) => setNameT(e.target.value)}
                             />
                         </InputGroup>
-						 <Form.Label>Тел.</Form.Label>
+                        <Form.Label>Тел.</Form.Label>
                         <InputGroup >
-							<InputGroup.Text>+7</InputGroup.Text>
+                            <InputGroup.Text>+7</InputGroup.Text>
                             <Form.Control
                                 type="text"
                                 inputMode="numeric"
@@ -172,17 +169,17 @@ const validEndpoints = ["vodka", "beer", "whiskey", "cognac", "liquor", "tequill
                                 name='Nomer telefona'
                                 value={phoneT}
                                 onChange={(e) => {
-                                        setPhoneT(e.target.value)
-									if(!reg.test(e.target.value)){
-										setSpanError('заполните номер телефона правильно')
-									}else{
-										setSpanError('')
-									}
+                                    setPhoneT(e.target.value)
+                                    if (!reg.test(e.target.value)) {
+                                        setSpanError('заполните номер телефона правильно')
+                                    } else {
+                                        setSpanError('')
+                                    }
                                 }}
                                 required
                             />
-							</InputGroup>
-						<span style={{color: 'red'}}>{spanError}</span>
+                        </InputGroup>
+                        <span style={{ color: 'red' }}>{spanError}</span>
                     </Form>
                 </Modal.Body>
                 <Modal.Footer>
